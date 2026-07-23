3e71616603ead9eeec78447cd7cd33c335c01bc8
sislam2005 <shujaaul@icloud.com>
2026-07-23 11:51:51 +0100

    Update SHEETS_URL to new Power BI API endpoint

--- usb.bat
+++ usb.bat
@@ -4,7 +4,7 @@
 ::  Edit the 3 lines below, copy to USB, done.
 :: ============================================================
  
-set SHEETS_URL=https://script.google.com/macros/s/AKfycbzszdfs_6r9JSblLJwT14h79hR9Ufj5ZrB3aanNLB2N4pjuaF8epylp6zKpTB2VclC2/exec
+set SHEETS_URL=https://api.powerbi.com/beta/186f8545-468f-49e5-9c70-379e23e2af14/datasets/a1cab86f-9a10-48d1-8c3c-9a8209df20eb/rows?experience=power-bi&key=myo4adHlLfblWnF7fGGyaYC1VlZjPqlPz0BLj8VmDbYfOTPHTv0gVenDD0njLLVrB%2BPgqZUD9CwwDH9mHGmJaA%3D%3D
 set MARKETING_URL=https://www.khipuawareness.co.uk/awareness/c8803bb920f32a6582260029c7404d983055d89355d291102c9fd2bea82e93e9/11/index.html
 set USB_ID=USB-001
 set SECRET=@kDeGGisN9UW
